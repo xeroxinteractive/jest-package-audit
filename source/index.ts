9e2503de0590bb0cfe72fe391e3819f9d901743d
Andrew Leedham <AndrewLeedham@Outlook.com>
2019-05-13 19:11:15 +0100

    feat: log error if jest can’t be extended

--- source/index.ts
+++ source/index.ts
@@ -2,4 +2,8 @@ import { toPassPackageAudit } from './matchers';
 
 if (expect !== undefined) {
   expect.extend({ toPassPackageAudit });
+} else {
+  console.error(
+    "jest-package-audit: Make sure jest is installed, couldn't install matcher."
+  );
 }
